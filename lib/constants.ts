5eb5d4d7bbebe1cf2635198ec333338edced17bd
Nicolas Vivar <nicolas@huckleberry-labs.com>
2024-11-26 19:49:28 -0500

    Deploy new CV version

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Nicolas Vivar Davila',
+  title: 'SRE Engineer',
+  description: 'Professional portfolio and CV of Nicolas Vivar Davila, SRE Engineer specializing in DevOps and cloud technologies.',
+  url: 'https://cv.nicko.cc',
+  ogImage: 'https://cv.nicko.cc/og.png',
+  links: {
+    github: 'https://github.com/nickovivar',
+    linkedin: 'https://linkedin.com/in/nickovivar',
+    email: 'mailto:nickovivar@gmail.com',
+    credly: 'https://www.credly.com/badges/c98e3f68-440b-4c3c-98b5-0c24a6f657e2/public_url'
+  }
+};
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 module.exports = nextConfig;
